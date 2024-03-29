62793d4c97add6112ee2dd2ce6f466ac4ea5feb6
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2024-03-29 18:33:34 -0400

    fix logic

--- apps/interceptor/src/vdj/getCurrentDeckState.ts
+++ apps/interceptor/src/vdj/getCurrentDeckState.ts
@@ -1,5 +1,5 @@
 import type { DeckState, VdjState } from 'models'
 
 export default function getCurrentDeckState(vdjState: VdjState): DeckState {
-  return vdjState.crossfader = 0.5 ? vdjState.rightDeck : vdjState.leftDeck
+  return vdjState.crossfader > 0.5 ? vdjState.rightDeck : vdjState.leftDeck
 }
