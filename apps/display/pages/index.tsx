94758ebabe15b6ed282e0d1d8ca9eee258bfd016
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2024-03-30 12:22:55 -0400

    new auth

--- apps/display/pages/index.tsx
+++ apps/display/pages/index.tsx
@@ -1,11 +1,8 @@
+'use client'
 import { Realtime } from 'ably'
+import { AblyProvider } from 'ably/react'
 import { disableBodyScroll } from 'body-scroll-lock'
-import {
-  AblyChannel,
-  AblyEvent,
-  DisplayUpdateNotification,
-  SimpleTrackInfo
-} from 'models'
+import { DisplayUpdateNotification, SimpleTrackInfo } from 'models'
 import { useEffect, useRef, useState } from 'react'
 import styles from '../styles/index.module.scss'
 
@@ -14,25 +11,28 @@ export default function Home() {
   const [menuOpen, setMenuOpen] = useState(false)
   const [simpleTrackInfo, setSimpleTrackInfo] = useState<SimpleTrackInfo>()
   const mainRef = useRef<HTMLDivElement>(null)
+  const client = new Realtime({
+    authUrl: 'https://project-448/api/ably-auth'
+  })
 
   useEffect(() => {
     disableBodyScroll(mainRef.current as HTMLElement)
-    const ablyClient = new Realtime(
-      'wYnVEA.Ge4Eag:AhrNqn5M9oxZxGqJk6vwz1BYZF419EnDmjpLOD5r9_0'
-    )
-    const ably = ablyClient.channels.get(AblyChannel.MAIN)
-    ably.publish(AblyEvent.JOIN, {}).then()
-    ably
-      .subscribe(AblyEvent.DISPLAY_UPDATE, async notification => {
-        handleDisplayUpdateNotification(
-          notification.data as DisplayUpdateNotification
-        )
-      })
-      .then()
-    return () => {
-      ably.unsubscribe()
-      ablyClient.close()
-    }
+    // const ablyClient = new Realtime(
+    //   'wYnVEA.Ge4Eag:AhrNqn5M9oxZxGqJk6vwz1BYZF419EnDmjpLOD5r9_0'
+    // )
+    // const ably = ablyClient.channels.get(AblyChannel.MAIN)
+    // ably.publish(AblyEvent.JOIN, {}).then()
+    // ably
+    //   .subscribe(AblyEvent.DISPLAY_UPDATE, async notification => {
+    //     handleDisplayUpdateNotification(
+    //       notification.data as DisplayUpdateNotification
+    //     )
+    //   })
+    //   .then()
+    // return () => {
+    //   ably.unsubscribe()
+    //   ablyClient.close()
+    // }
   }, [])
 
   function handleDisplayUpdateNotification(
@@ -43,79 +43,84 @@ export default function Home() {
   }
 
   return (
-    <main className={styles.main} ref={mainRef}>
-      <div className={styles.topbar}>
-        <img
-          className={styles.menuIcon}
-          src={menuOpen ? '/close.svg' : '/meatballs-menu.svg'}
-          alt='menu'
-          onClickCapture={() => {
-            setMenuOpen(curr => !curr)
-          }}
-        />
-      </div>
-      <div
-        className={styles.menuIsland}
-        style={{
-          right: menuOpen ? '73vw' : 0
-        }}
-      >
-        <div className={styles.menuContent}>
-          <h1>Project 448</h1>
-          <h2>By Shun</h2>
-          <ul>
-            <li>
-              Request a Song <i>(Coming soon)</i>
-            </li>
-            <li>
-              Contribute on{' '}
-              <a target='_blank' href='https://github.com/shunueda/project-448'>
-                GitHub
-              </a>
-            </li>
-            <li>
-              Make a donation:{' '}
-              <a
-                target='_blank'
-                href='https://account.venmo.com/pay?recipients=Kirk-Yap'
-              >
-                @Kirk-Yap
-              </a>
-            </li>
-          </ul>
+    <AblyProvider client={client}>
+      <main className={styles.main} ref={mainRef}>
+        <div className={styles.topbar}>
+          <img
+            className={styles.menuIcon}
+            src={menuOpen ? '/close.svg' : '/meatballs-menu.svg'}
+            alt='menu'
+            onClickCapture={() => {
+              setMenuOpen(curr => !curr)
+            }}
+          />
         </div>
-      </div>
-      <div className={styles.lyricsArea}>
-        {lines.map((line, i) => (
-          <div className={styles.shelf}>
-            <p
-              className={styles.line}
-              style={{
-                color:
-                  i < Math.floor(lines.length / 2)
-                    ? '#3f3f3f'
-                    : `rgba(220, 220, 220, ${1 - Math.abs(i - Math.floor(lines.length / 2)) / 2.2})`
-              }}
-            >
-              {line}
+        <div
+          className={styles.menuIsland}
+          style={{
+            right: menuOpen ? '73vw' : 0
+          }}
+        >
+          <div className={styles.menuContent}>
+            <h1>Project 448</h1>
+            <h2>By Shun</h2>
+            <ul>
+              <li>
+                Request a Song <i>(Coming soon)</i>
+              </li>
+              <li>
+                Contribute on{' '}
+                <a
+                  target='_blank'
+                  href='https://github.com/shunueda/project-448'
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                Make a donation:{' '}
+                <a
+                  target='_blank'
+                  href='https://account.venmo.com/pay?recipients=Kirk-Yap'
+                >
+                  @Kirk-Yap
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.lyricsArea}>
+          {lines.map((line, i) => (
+            <div className={styles.shelf}>
+              <p
+                className={styles.line}
+                style={{
+                  color:
+                    i < Math.floor(lines.length / 2)
+                      ? '#3f3f3f'
+                      : `rgba(220, 220, 220, ${1 - Math.abs(i - Math.floor(lines.length / 2)) / 2.2})`
+                }}
+              >
+                {line}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className={styles.trackInfoArea}>
+          <div
+            className={styles.coverArt}
+            style={{
+              backgroundImage: `url(${simpleTrackInfo?.coverArtUrl || ''})`
+            }}
+          />
+          <div className={styles.textarea}>
+            <p className={styles.musicTitle}>{simpleTrackInfo?.title}</p>
+            <p className={styles.musicArtist}>
+              {simpleTrackInfo?.artist} · {simpleTrackInfo?.album}
             </p>
           </div>
-        ))}
-      </div>
-      <div className={styles.trackInfoArea}>
-        <div
-          className={styles.coverArt}
-          style={{
-            backgroundImage: `url(${simpleTrackInfo?.coverArtUrl || ''})`
-          }}
-        />
-        <div className={styles.textarea}>
-          <p className={styles.musicTitle}>{simpleTrackInfo?.title}</p>
-          <p className={styles.musicArtist}>
-            {simpleTrackInfo?.artist} · {simpleTrackInfo?.album}
-          </p>
         </div>
-      </div>
-    </main>
+      </main>
+    </AblyProvider>
   )
 }
--- env.d.ts
+++ env.d.ts
@@ -7,6 +7,7 @@ declare global {
       SPOTIFY_CLIENT_SECRET: string
       VDJ_DIR: string
       ABLY_API_KEY: string
+      ABLY_API_SERVER_KEY: string
     }
   }
 }
