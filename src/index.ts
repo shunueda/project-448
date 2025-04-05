4abd7b3a3a3de5811a40f7fdc05e27785dfb16cf
Shun Ueda <me@shu.nu>
2025-04-04 21:42:45 -0400

    refactor: bump

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,21 @@ import { writeFile } from 'node:fs/promises'
 import { homedir } from 'node:os'
 import { join } from 'node:path'
 import { pipeline } from 'node:stream/promises'
+import { mkdirp } from 'mkdirp'
 import { directory, playlists } from '~/assets/config.json'
 import { writeMetadata } from '#ffmpeg/writeMetadata'
 import { getPlaylistItems } from '#spotify/getPlaylistItems'
 import { createVdjFolder } from '#virtualdj/createVdjFolder'
 import { download } from '#youtube/download'
 
+const tracksDir = join(homedir(), directory.virtualdj, 'Tracks')
+await mkdirp(tracksDir)
+
 for (const { id, name } of playlists) {
   const tracks = await getPlaylistItems(id)
   const paths = new Set<PathLike>()
   for (const track of tracks) {
-    const path = join(
-      homedir(),
-      directory.virtual_dj,
-      'Tracks',
-      `${track.id}.m4a`
-    )
+    const path = join(tracksDir, `${track.id}.m4a`)
     if (existsSync(path)) {
       paths.add(path)
       continue
@@ -40,7 +39,7 @@ for (const { id, name } of playlists) {
   }
   const vdjfolder = join(
     homedir(),
-    directory.virtual_dj,
+    directory.virtualdj,
     'MyLists',
     `${name}.vdjfolder`
   )
